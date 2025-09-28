1c420e1ce9254f7ea5967dcc4f6fa522286969df
Riski Agung <riski97@gmail.com>
2025-09-28 14:05:10 +0700

    update

--- src/app/[lang]/(auth)/layout.tsx
+++ src/app/[lang]/(auth)/layout.tsx
@@ -1,18 +1,6 @@
-"use client";
-
 import React from "react";
 import Image from "next/image";
-import { cn } from "@/lib/utils";
 import Logo from "@/assets/logo.png";
-import AuthRedirect from "@/context/AuthRedirect";
-import ThemeSwitcher from "@/components/ThemeSwitcher";
-import {
-  Card,
-  CardHeader,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
-import { TypographyH2, TypographyP } from "@/components/ui/typography";
 
 export default function AuthRootLayout({
   children,
@@ -20,36 +8,8 @@ export default function AuthRootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <AuthRedirect redirectType="auth">
-      <div className="min-h-screen flex items-center justify-center bg-muted py-6 px-4 sm:px-6 lg:px-8 relative">
-        <div className="absolute top-4 right-4 z-10">
-          <ThemeSwitcher />
-        </div>
-        <Card className="max-w-md w-full">
-          <CardHeader className="text-center">
-            <div className="flex justify-center">
-              <Image
-                src={Logo}
-                alt="Cashierly Logo"
-                width={100}
-                height={100}
-                className={cn("w-auto", "mb-2")}
-                priority
-              />
-            </div>
-            <TypographyH2 className="mt-6">Cashierly</TypographyH2>
-            <TypographyP className="mt-2">
-              Manage your business transactions efficiently
-            </TypographyP>
-          </CardHeader>
-          <CardContent>{children}</CardContent>
-          <CardFooter className="flex flex-col gap-2 text-center">
-            <TypographyP className="text-xs">
-              &copy; {new Date().getFullYear()} Cashierly. All rights reserved.
-            </TypographyP>
-          </CardFooter>
-        </Card>
-      </div>
-    </AuthRedirect>
+    <div className="min-h-screen flex items-center justify-center bg-muted py-6 px-4 sm:px-6 lg:px-8 relative">
+      <div>{children}</div>
+    </div>
   );
 }
