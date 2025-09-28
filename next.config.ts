0f8d1b1e9f5cc4d4884797c6f2df740a2feb39d0
Riski Agung <riski97@gmail.com>
2025-09-28 10:55:23 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/en",
+        permanent: false,
+      },
+    ];
+  },
+
   /* config options here */
 };
 
